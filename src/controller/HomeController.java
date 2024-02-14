87b638302f1d13569cf91e198589904355aa62ca
elleyfolks <elleyfolks@gmail.com>
2024-02-14 16:27:05 -0700

    Added tab pane for having multiple table views.
    Idea is to have a pane with customers table, and a pane with appointments table.

--- src/controller/HomeController.java
+++ src/controller/HomeController.java
@@ -16,18 +16,19 @@ import java.util.ResourceBundle;
 
 public class HomeController implements Initializable {
     @FXML
-    TableView<ObservableList<String>> homeTable = new TableView<>();
+    TableView<ObservableList<String>> customersTable = new TableView<>();
+
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         try {
 
             // Set padding to create a border around the TableView
-            homeTable.setPadding(new javafx.geometry.Insets(20));
+            customersTable.setPadding(new javafx.geometry.Insets(20));
 
             PreparedStatement statement = ManageQuery.createSelectQuery("customers");
             ResultSet results = ManageQuery.getQueryResults(statement);
-            LoadTable.loadData(homeTable, results);
+            LoadTable.loadData(customersTable, results);
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
