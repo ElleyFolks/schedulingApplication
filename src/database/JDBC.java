bf8fb4a25af78f8574219e62df8f9547eae52e70
elleyfolks <elleyfolks@gmail.com>
2024-03-04 13:25:05 -0700

    WIP Fixed DB connection.

--- src/database/JDBC.java
+++ src/database/JDBC.java
@@ -10,22 +10,13 @@ public class JDBC {
 
     private static final String location = "//localhost/";
 
-    // WGU Database - Dear EVALUATOR, please fill out this information below for your connection. Thanks!
-    private static final String ipAddress = "://wgudb.ucertify.com:3306/";
-
-    private static final String dbName = "WJ06YG0";// name of DB
-    private static final String username = "sqlUser";
-    private static final String password = "Passw0rd!";
-
     // Personal Database for my own development.
-    //private static final String ipAddress = "://192.168.1.210:3306/";
-    //private static final String dbName = "SchedulingApplication";
-    //private static final String username = DatabaseConfig.getUsername();
-    //private static final String password = DatabaseConfig.getPassword();
+    private static final String ipAddress = "://192.168.1.210:3306/";
+    private static final String dbName = "SchedulingApplication";
+    private static final String username = DatabaseConfig.getUsername();
+    private static final String password = DatabaseConfig.getPassword();
 
-
-    //private static final String jdbcURL = protocol + vendorName + ipAddress + dbName + "?connectionTimeZone=SERVER"; // used for personal set up
-    private static final String jdbcURL = protocol + vendorName + location + ipAddress + dbName + "?connectionTimeZone=SERVER";
+    private static final String jdbcURL = protocol + vendorName + ipAddress + dbName + "?connectionTimeZone=SERVER"; // used for personal set up
     private static final String MYSQLJDBCDriver = "com.mysql.cj.jdbc.Driver";
     private static Connection conn = null;
 
