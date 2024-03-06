b79fa42e91fc108ddbb6d68a12cd45119449330d
elleyfolks <116213000+ElleyFolks@users.noreply.github.com>
2024-03-06 13:40:40 -0700

    Changed how reports are implemented.
    Now using list view instead of table view, and streams to group / organize a list of class objects.

--- src/controller/HomeController.java
+++ src/controller/HomeController.java
@@ -1,7 +1,6 @@
 package controller;
 
 import database.CustomerQuery;
-import database.HelperQuery;
 import helper.Alerts;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXMLLoader;
@@ -18,10 +17,7 @@ import model.Customer;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
-import java.util.ResourceBundle;
+import java.util.*;
 
 /**
  * Class that contains event handlers, controller methods,
@@ -51,16 +47,10 @@ public class HomeController implements Initializable {
     private RadioButton appointmentContactReport;
 
     @FXML
-    private RadioButton appointmentMonthReport;
-
-    @FXML
-    private RadioButton appointmentTypeReport;
+    private RadioButton appointmentMonthTypeReport;
 
     @FXML
     private RadioButton appointmentCountryReport;
-
-    @FXML
-    private Label reportResultLabel;
 
     public static Appointment appointmentToModify;
 
@@ -73,19 +63,7 @@ public class HomeController implements Initializable {
     ObservableList<Customer> customers = FXCollections.observableArrayList();
 
     @FXML
-    private Label reportComboBoxLabel;
-
-    @FXML
-    private ComboBox<String> reportComboBox;
-
-    @FXML
-    private TableView<Appointment> reportTableView;
-
-    private Map<String, Integer> contactNameIdMap = new HashMap<>();
-
-    private Map<String, Integer> customerNameIdMap = new HashMap<>();
-
-    private Map<String, Integer> countryNameIdMap = new HashMap<>();
+    private ListView<String> reportListView;
 
     /**
      * Initializes the controller with the necessary setup for the main screen.
@@ -97,6 +75,7 @@ public class HomeController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         try {
+
             // populates customers table
             customersTable.setPadding(new javafx.geometry.Insets(20));
             CustomerQuery.formatCustomerTable(customersTable);
@@ -110,10 +89,6 @@ public class HomeController implements Initializable {
             AppointmentQuery.getAllAppointments(appointments, appointmentsTable);
 
             populateAppointmentTable();
-
-            // appointments by type selected by default
-            appointmentTypeReport.setSelected(true);
-            formatReport();
 
         } catch (Exception e) {
             throw new RuntimeException(e);
@@ -151,99 +126,30 @@ public class HomeController implements Initializable {
     }
 
     /**
-     * Formats the report view based on the selected report type.
-     * Clears existing data and populates the report combo box with appropriate options.
-     */
-    @FXML
-    private void formatReport(){
-
-        // clearing results because these can change
-        contactNameIdMap.clear();
-        customerNameIdMap.clear();
-        reportComboBox.getItems().clear();
-        reportResultLabel.setText("");
-
-        // populates reports combo box with appointment types
-        if(appointmentTypeReport.isSelected()){
-            reportComboBoxLabel.setText("Select type for report.");
-            HelperQuery.setAppointmentTypes(reportComboBox);
-        }
-
-        // populates reports combo box with months
-        if(appointmentMonthReport.isSelected()){
-            reportComboBoxLabel.setText("Select appointment month.");
-            for (int i = 1; i <= 12; i++) {
-                if(i<10){
-                    reportComboBox.getItems().add("0"+String.valueOf(i));
-                }else{
-                    reportComboBox.getItems().add(String.valueOf(i));
-                }
-            }
-        }
-
-        // populates report combo box with contact names
-        if(appointmentContactReport.isSelected()){
-            reportComboBoxLabel.setText("Select contact.");
-            HelperQuery.setAppointmentContacts(reportComboBox, contactNameIdMap);
-        }
-
-        if(appointmentCountryReport.isSelected()){
-            reportComboBoxLabel.setText("Select country.");
-            CustomerQuery.getCountryNameID(reportComboBox,countryNameIdMap);
-        }
-    }
-
-    /**
      * Creates and displays the report based on the selected report type and options.
      * Resets the table view, then formats and populates the view with the relevant data.
      * Updates the report result label with the total number of records in the report.
      */
     @FXML
     private void createReport(){
-        if(appointmentTypeReport.isSelected() && reportComboBox.getValue() != null){
+
+        appointments = AppointmentQuery.getAllAppointments();
+        customers = CustomerQuery.getAllCustomers();
+
+        if(appointmentMonthTypeReport.isSelected()){
             // resets table view
-            reportTableView.getItems().clear();
-            reportTableView.getColumns().clear();
-
-            // formats and populates view with appointments of selected type
-            AppointmentQuery.formatAppointmentTable(reportTableView);
-            AppointmentQuery.getAppointmentsOfType(reportTableView,reportComboBox.getValue());
-            reportResultLabel.setText("Total number of appointments with type "+ reportComboBox.getValue()
-                    + " is "+ reportTableView.getItems().size());
+            reportListView.getItems().clear();
+            reportListView.setItems(helper.Reports.getMonthTypeReport(appointments));
         }
 
-        if(appointmentMonthReport.isSelected() && reportComboBox.getValue() != null){
-            // resets table view
-            reportTableView.getItems().clear();
-            reportTableView.getColumns().clear();
-
-            // formats and populates view with appointments of selected month
-            AppointmentQuery.formatAppointmentTable(reportTableView);
-            AppointmentQuery.getAppointmentsOfMonth(reportTableView, reportComboBox.getValue());
-            reportResultLabel.setText("Total number of appointments in the month of "+ reportComboBox.getValue()
-                    + " is "+ reportTableView.getItems().size());
+        if(appointmentContactReport.isSelected()){
+            reportListView.getItems().clear();
+            reportListView.setItems(helper.Reports.getContactReport(appointments));
         }
 
-        if(appointmentContactReport.isSelected() && reportComboBox.getValue() != null){
-            reportTableView.getItems().clear();
-            reportTableView.getColumns().clear();
-
-            // formats and populates view with appointments of selected month
-            AppointmentQuery.formatAppointmentTable(reportTableView);
-            AppointmentQuery.getAppointmentsOfContactID(reportTableView, contactNameIdMap.get(reportComboBox.getValue()));
-            reportResultLabel.setText("Total number of appointments for contact "+ reportComboBox.getValue()
-            + " is " + reportTableView.getItems().size());
-        }
-
-        if(appointmentCountryReport.isSelected() && reportComboBox.getValue() != null){
-            reportTableView.getItems().clear();
-            reportTableView.getColumns().clear();
-
-            // formats and populates view with appointments of selected month
-            AppointmentQuery.formatAppointmentTable(reportTableView);
-            AppointmentQuery.getAppointmentsWithCountryID(reportTableView, countryNameIdMap.get(reportComboBox.getValue()));
-            reportResultLabel.setText("Total number of appointments for country "+ reportComboBox.getValue()
-                    + " is " + reportTableView.getItems().size());
+        if(appointmentCountryReport.isSelected()){
+            reportListView.getItems().clear();
+            reportListView.setItems(helper.Reports.getCountryReport(customers));
         }
     }
 
@@ -378,35 +284,35 @@ public class HomeController implements Initializable {
     void onRemoveCustomerAction() {
         Customer selectedCustomer = customersTable.getSelectionModel().getSelectedItem();
 
-            // cannot delete item if nothing is selected
-            if (selectedCustomer == null) {
-                Alerts.showErrorAlert("noSelectedItem", "customer");
-            }
-            else {
-                // checks if customer has appointments
-                if (CustomerQuery.customerHasAppointment(selectedCustomer.getCustomerId())) {
+        // cannot delete item if nothing is selected
+        if (selectedCustomer == null) {
+            Alerts.showErrorAlert("noSelectedItem", "customer");
+        }
+        else {
+            // checks if customer has appointments
+            if (CustomerQuery.customerHasAppointment(selectedCustomer.getCustomerId())) {
 
-                    Alerts.showErrorAlert("customerHasAppointment", selectedCustomer.getCustomerFullName());
-                } else {
+                Alerts.showErrorAlert("customerHasAppointment", selectedCustomer.getCustomerFullName());
+            } else {
 
-                    Optional<ButtonType> result = Alerts.showConfirmAlert("deleteConfirm", "customer");
+                Optional<ButtonType> result = Alerts.showConfirmAlert("deleteConfirm", "customer");
 
-                    if (result.isPresent() && result.get() == ButtonType.OK) {
-                        boolean successfulDelete = CustomerQuery.removeCustomer(selectedCustomer.getCustomerId());
+                if (result.isPresent() && result.get() == ButtonType.OK) {
+                    boolean successfulDelete = CustomerQuery.removeCustomer(selectedCustomer.getCustomerId());
 
-                        if (successfulDelete) {
-                            System.out.println("Customer successfully deleted");
-                            String deleteMessage = String.valueOf(selectedCustomer.getCustomerId()) + " " + selectedCustomer.getCustomerFullName();
-                            Alerts.showInfoAlert("successfulDelete", "customer", deleteMessage);
+                    if (successfulDelete) {
+                        System.out.println("Customer successfully deleted");
+                        String deleteMessage = String.valueOf(selectedCustomer.getCustomerId()) + " " + selectedCustomer.getCustomerFullName();
+                        Alerts.showInfoAlert("successfulDelete", "customer", deleteMessage);
 
-                            // clearing observable list and updating table
-                            customers.clear();
-                            CustomerQuery.getAllCustomers(customers, customersTable);
-                        }
+                        // clearing observable list and updating table
+                        customers.clear();
+                        CustomerQuery.getAllCustomers(customers, customersTable);
                     }
                 }
             }
         }
+    }
 
 
     /**
